5db9ab2b7f8e6e4ae81893535aee8510337fe820
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-11-29 23:33:15 +0000

    Added a node counter

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_nodes - function that counts non-empty nodes
+*
+* @tree: the node
+* Return: The number of non empty nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
+{
+	int count;
+	
+	if (tree == NULL)
+		return (0);
+	
+	count = 0;
+
+	if (tree->left || tree->right)
+		count++;
+	return (count);
+}
