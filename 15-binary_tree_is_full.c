196a49792b57fcfe1c57ab391824ef4ae285b818
Obed101 <obedamoako92@gmail.com>
2021-12-01 02:30:55 -0800

    commented to test

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,11 +15,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if ((tree->left) && (tree->right))
 	{
-		if (binary_tree_is_full(tree->left) &&
+/*		if (binary_tree_is_full(tree->left) &&
 		    binary_tree_is_full(tree->right))
-			return (TRUE);
-		return (FALSE);
-	}
+*/			return (TRUE);
+/*		return (FALSE);
+ */	}
 	if ((!tree->left && tree->right) || (tree->left && !tree->right))
 		return (FALSE);
 	return (FALSE);
