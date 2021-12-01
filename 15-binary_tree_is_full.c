25f736508878175eb84c29bbe4df325665b2c961
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-01 10:06:08 +0000

    fixed compilation

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,6 +18,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
 			return (TRUE);
 	}
-	if ((!tree->left && tree->right) || tree->left && !tree->right)
+	if ((!tree->left && tree->right) || (tree->left && !tree->right))
 		return (FALSE);
 }
