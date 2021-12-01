ed77266a4df424c4575e0add31bf10e7720cf035
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-12-01 18:29:04 +0000

    Fixed compilation finally

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -23,9 +23,11 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 		tree->left->parent = tmp;
 	}
 	else
+	{
 		tmp->right = NULL;
 		tmp->parent = tree;
 		tree->left = tmp;
+	}
 
 	if (par != NULL)
 	{
